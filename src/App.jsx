1013ab90a81d7a0512bd212f763630da9955bca3
Pragya Prabir Ranjan Deka <www.pprd@gmail.com>
2024-03-18 23:29:52 +0530

    ADD:Gallery Page animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Gallery from "./Pages/Gallery/Gallery";
+
 const App = () => {
   return (
     <main>
       <h1 className="text-4xl font-bold underline text-red-600">Hello world!</h1>
+      <Gallery />
     </main>
   );
 };
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,5 +1,68 @@
+import React from "react";
+import styles from "./Gallery.module.scss";
 const Gallery = () => {
-  return <div></div>;
+  const images = [
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    "image4.jpg",
+    "image5.jpg",
+    "image6.jpg",
+    "image7.jpg",
+    "image8.jpg",
+    "image9.jpg",
+    "image10.jpg",
+  ];
+
+  return (
+    <div className={styles.AppContainer}>
+      <div className={styles.Wrapper}>
+        <div className={styles.ImgRow1}>
+          {images.map((element, index) => {
+            return (
+              <img
+                src={element}
+                alt=""
+                className="img"
+                style={{
+                  background: index % 2 === 0 ? "yellow" : "blue",
+                  padding: "10px",
+                  width: "clamp(100px,20vw,400px)",
+                  border: "2px solid black",
+                }}
+              />
+            );
+          })}
+        </div>
+
+        <div>
+          <img
+            src=""
+            style={{ background: "red", height: "30vh", width: "40vw" }}
+            alt=""
+          />
+        </div>
+
+        <div className={styles.ImgRow2}>
+          {images.map((element, index) => {
+            return (
+              <img
+                src={element}
+                alt=""
+                className="img"
+                style={{
+                  background: index % 2 === 0 ? "yellow" : "blue",
+                  padding: "10px",
+                  width: "clamp(100px,20vw,400px)",
+                  border: "2px solid black",
+                }}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
