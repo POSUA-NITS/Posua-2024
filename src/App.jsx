e858127172f2f68a35ee9b45ce814c401ab82457
barnilsarma <barnilsarma@gmail.com>
2024-03-18 18:55:12 +0530

    add background images(few modifications left)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Events, Home } from "./Pages/index";
 const App = () => {
   return (
     <main>
-      <h1 className="text-4xl font-bold underline text-red-600">Navbar</h1>
+      {/* <h1 className="text-4xl font-bold underline text-red-600">Navbar</h1> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
