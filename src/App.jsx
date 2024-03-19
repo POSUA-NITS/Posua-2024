5ae915aff17dd94e1646c152cb92cfdc17d979b4
webdotmilan <milans708688@gmail.com>
2024-03-19 08:12:17 +0530

    Gallery Page completed with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Gallery from "./Pages/Gallery/Gallery";
 const App = () => {
   return (
     <main>
-      <h1 className="text-4xl font-bold underline text-red-600">Hello world!</h1>
+      {/* <h1 className="text-4xl font-bold underline text-red-600">Hello world!</h1> */}
       <Gallery />
     </main>
   );
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -2,63 +2,54 @@ import React from "react";
 import styles from "./Gallery.module.scss";
 const Gallery = () => {
   const images = [
-    "image1.jpg",
-    "image2.jpg",
-    "image3.jpg",
-    "image4.jpg",
-    "image5.jpg",
-    "image6.jpg",
-    "image7.jpg",
-    "image8.jpg",
-    "image9.jpg",
-    "image10.jpg",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788035/Property_1_1_ivsgaw.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788013/Property_1_2_d79j4h.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788017/Property_1_3_o4ymlp.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788016/Property_1_4_zqukek.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788037/Property_1_5_ui5l7q.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788058/Property_1_6_axme5t.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788029/Property_1_7_jbhwhm.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788035/Property_1_1_ivsgaw.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788056/Property_1_8_nfuvh5.jpg?_s=public-apps",
+    "https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710788017/Property_1_3_o4ymlp.jpg?_s=public-apps",
   ];
 
   return (
     <div className={styles.AppContainer}>
       <div className={styles.Wrapper}>
         <div className={styles.ImgRow1}>
-          {images.map((element, index) => {
-            return (
+          {images.map((imageUrl, index) => (
+            <div key={`image-${imageUrl}`} className={styles.ImgRow12}>
               <img
-                src={element}
-                alt=""
+                src={imageUrl}
+                alt={`Property  ${index + 1}`}
                 className="img"
-                style={{
-                  background: index % 2 === 0 ? "yellow" : "blue",
-                  padding: "10px",
-                  width: "clamp(100px,20vw,400px)",
-                  border: "2px solid black",
-                }}
+                style={{ padding: "1rem" }}
               />
-            );
-          })}
+            </div>
+          ))}
         </div>
 
-        <div>
+        <div className={styles.single}>
           <img
-            src=""
-            style={{ background: "red", height: "30vh", width: "40vw" }}
-            alt=""
+            className="img"
+            src="https://res.cloudinary.com/dlyo7ec6c/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710793889/Group_2608609_1_p0nsnp.jpg?_s=public-apps"
+            alt="Featured property "
+            // style={{ height: "26vh", width: "33vw" }}
           />
         </div>
 
         <div className={styles.ImgRow2}>
-          {images.map((element, index) => {
-            return (
+          {images.map((imageUrl, index) => (
+            <div key={`image-row2-${imageUrl}`} className={styles.ImgRow12}>
               <img
-                src={element}
-                alt=""
+                src={imageUrl}
+                alt={`Property  row 2, item ${index + 1}`}
                 className="img"
-                style={{
-                  background: index % 2 === 0 ? "yellow" : "blue",
-                  padding: "10px",
-                  width: "clamp(100px,20vw,400px)",
-                  border: "2px solid black",
-                }}
+                style={{ padding: "1rem" }}
               />
-            );
-          })}
+            </div>
+          ))}
         </div>
       </div>
     </div>
