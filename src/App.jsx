94c9259793cda834356f23eddfc44755b3046674
satyaaa111 <juman60000@gmail.com>
2024-03-21 19:58:30 +0530

    removed teampage from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import Team from "./Pages/Team/Team";
 const App = () => {
   return (
     <main>
-      {/* <h1 className="text-4xl font-bold underline text-red-600">Hello world!</h1> */}
-      <Team />
+      <h1 className="text-4xl font-bold underline text-red-600">Hello world!</h1>
     </main>
   );
 };
