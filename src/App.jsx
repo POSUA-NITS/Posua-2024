0c8f9e71ed5b667a7d8d436178526347d039c2ef
GracieRuth <gracieruth2505@gmail.com>
2024-03-22 15:56:42 +0530

    ADD:sponsors page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Sponsors from "./Pages/Sponsors/Sponsors";
 const App = () => {
   return (
     <main>
       <h1 className="text-4xl font-bold underline text-red-600">Hello world!</h1>
+      <Sponsors />
     </main>
   );
 };
--- src/Pages/Sponsors/Sponsors.jsx
+++ src/Pages/Sponsors/Sponsors.jsx
@@ -1,5 +1,178 @@
-const Sponsors = () => {
-  return <div></div>;
+// SponsporsPage
+import React from "react";
+import "./sponsors.scss";
+const SponsorPage = () => {
+  return (
+    <div>
+      <body>
+        <div className="first">
+          <div className="heading">
+            <img
+              src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711041756/OBJECTS_y61orj.png"
+              alt=""
+            />
+          </div>
+          <div className="sponsorContainer">
+            <div className="sponsor"></div>
+            <div className="sponsor"></div>
+            <div className="sponsor"></div>
+            <div className="sponsor"></div>
+            <div className="sponsor"></div>
+          </div>
+          <div className="bihu">
+            <img
+              src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711041851/magh_bihu_1_2_edzkl9.png"
+              alt=""
+            />
+          </div>
+        </div>
+        <div>
+          <div className="heading">
+            <img
+              src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711041758/Group_2608570_lbnsan.png"
+              alt=""
+            />
+          </div>
+          <div className="sponsorContainer">
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098244/Hyundai_Logo_zqyig4.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098243/Pran_Logo_qkebp4.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098241/Oil_India-Logo_zwjxl7.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098241/NRL_Logo_sja6jq.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor" id="Indian_Oil">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711099239/Indian_Oil_Logo1_gx89of.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div className="cardContainer">
+          <div className="card">
+            <div className="inner">
+              <div className="number">
+                <p>01</p>
+              </div>
+              <div className="title">
+                <p>Marketing Opportunities</p>
+              </div>
+              <div className="content">
+                <p>
+                  National Institute of Technology,Silchar,an institute of national
+                  importance is pleased to announce that the institute is organising
+                  POSUA’22, the Rongali Bihu Festival of Assam.The entire team of POSUA
+                  2022 is dedicated to ensure maximum reach of our sponsors through the
+                  basic framework of the festival. We will make the best of efforts to
+                  promote our sponsoring organisations across a wide audience.The team
+                  will be popularizing the generous sponsors among students as well as
+                  faculty so that they can avail the services rendered by your
+                  organization. Your presence will be highly appreciated and would greatly
+                  help us in making this annual event of the Institute a grand success.The
+                  Institute will be grateful for your support and faith in us and would
+                  surely extend our gratitude towards your organisation. Hence,we ,the
+                  entire team of POSUA 2022 cordially invite you for sponsorship in this
+                  Annual Cultural Fest.
+                </p>
+              </div>
+              <div className="image">
+                <img
+                  src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711041820/Frame_26086392_nyx2rp.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="inner">
+              <div className="number">
+                <p>01</p>
+              </div>
+              <div className="title">
+                <p>Marketing Opportunities</p>
+              </div>
+              <div className="content">
+                <p>
+                  National Institute of Technology,Silchar,an institute of national
+                  importance is pleased to announce that the institute is organising
+                  POSUA’22, the Rongali Bihu Festival of Assam.The entire team of POSUA
+                  2022 is dedicated to ensure maximum reach of our sponsors through the
+                  basic framework of the festival. We will make the best of efforts to
+                  promote our sponsoring organisations across a wide audience.The team
+                  will be popularizing the generous sponsors among students as well as
+                  faculty so that they can avail the services rendered by your
+                  organization. Your presence will be highly appreciated and would greatly
+                  help us in making this annual event of the Institute a grand success.The
+                  Institute will be grateful for your support and faith in us and would
+                  surely extend our gratitude towards your organisation. Hence,we ,the
+                  entire team of POSUA 2022 cordially invite you for sponsorship in this
+                  Annual Cultural Fest.
+                </p>
+              </div>
+              <div className="image">
+                <img
+                  src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711041768/Frame_26086391_sgjt4z.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="inner">
+              <div className="number">
+                <p>01</p>
+              </div>
+              <div className="title">
+                <p>Marketing Opportunities</p>
+              </div>
+              <div className="content">
+                <p>
+                  National Institute of Technology,Silchar,an institute of national
+                  importance is pleased to announce that the institute is organising
+                  POSUA’22, the Rongali Bihu Festival of Assam.The entire team of POSUA
+                  2022 is dedicated to ensure maximum reach of our sponsors through the
+                  basic framework of the festival. We will make the best of efforts to
+                  promote our sponsoring organisations across a wide audience.The team
+                  will be popularizing the generous sponsors among students as well as
+                  faculty so that they can avail the services rendered by your
+                  organization. Your presence will be highly appreciated and would greatly
+                  help us in making this annual event of the Institute a grand success.The
+                  Institute will be grateful for your support and faith in us and would
+                  surely extend our gratitude towards your organisation. Hence,we ,the
+                  entire team of POSUA 2022 cordially invite you for sponsorship in this
+                  Annual Cultural Fest.
+                </p>
+              </div>
+              <div className="image">
+                <img
+                  src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711041752/Frame_2608639_hwrfdx.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
 };
 
-export default Sponsors;
+export default SponsorPage;
