0e6af70115ac8051e413d3bb28f1d2ec602cec0e
debismaBorah27 <debismap.borah@gmail.com>
2024-03-23 17:37:02 +0530

    responsive footer and navbar

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -25,75 +25,80 @@ const Footer = () => {
       //   }}
       className={`${styles.Container} ${fontStyles.PhilosopherFont}`}
     >
-      <div
-        className={styles.main}
-        // style={{
-        //     display:'flex',
-        //     flexDirection:'column',
-        //     justifyContent:'center',
-        //     alignItems:'center',
-        //     height:'100%',
-        //     width:'100%',
-        //     gap : "5rem"
-        // }}
-      >
+      <div className={styles.posua}>
         <img
           src="https://res.cloudinary.com/dclhahfvz/image/upload/v1710953023/New_Posua_logo_kyknan.png"
           alt="Posua Logo"
         />
-        <div
-          className={styles.content}
-          // style={{
-          //     display:'flex',
-          //     justifyContent:'space-evenly',
-          //     alignItems:'center',
-          //     width :"100%",
-          //     fontFamily:'Roboto',
-          //     fontWeight:'400',
-          //     color:'#fff'
+      </div>
+      <div
+        className={styles.content}
+        // style={{
+        //     display:'flex',
+        //     justifyContent:'space-evenly',
+        //     alignItems:'center',
+        //     width :"100%",
+        //     fontFamily:'Roboto',
+        //     fontWeight:'400',
+        //     color:'#fff'
 
-          // }}
-        >
-          <div className={styles.Contact}>
-            <p>Contacts</p>
-            <p>1234567890</p>
-            <p>9876543210</p>
+        // }}
+      >
+        <div className={styles.Contact}>
+          <div className="Cntct" style={{ textDecoration: "underline" }}>
+            Contacts
           </div>
-          <div className={styles.address}>
-            <p>Address</p>
-            <p>Silchar, Assam</p>
+          <div>
+            {" "}
+            <a href="tel:1234567890">XYZ , 1234567890</a>
           </div>
-          <div className={styles.socialmedia}>
-            <p>Social Media</p>
-            {/* <p>Instagram - @posua_insta</p> */}
-            <p>
-              <a
-                href="https://www.instagram.com/posua_nits?igsh=MXR6a29raDJhaDFiaQ=="
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Instagram{" "}
-              </a>
-            </p>
-            {/* <p>Facebook - facebook.com/posua</p> */}
-            <p>
-              <a
-                href="https://www.facebook.com/nitsposua?mibextid=ZbWKwL"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Facebook{" "}
-              </a>
-            </p>
-          </div>
-          <div className={styles.mail}>
-            <p>Mail</p>
-            <p>borhomthuri.posua@gmail.com</p>
+          <div>
+            <a href="tel:9876543210">xyz , 9876543210</a>
           </div>
         </div>
-
-        {/* <img src='https://res.cloudinary.com/dclhahfvz/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710923131/footer_final_mj5rba.jpg?_s=public-apps' alt=''/> */}
+        <div className={styles.address}>
+          <div className="Add" style={{ textDecoration: "underline" }}>
+            Address
+          </div>
+          <div>Silchar, Assam</div>
+        </div>
+        <div className={styles.socialmedia}>
+          <div className="Soc" style={{ textDecoration: "underline" }}>
+            Social
+          </div>
+          {/* <p>Instagram - @posua_insta</p> */}
+          <div>
+            <a
+              href="https://www.instagram.com/posua_nits?igsh=MXR6a29raDJhaDFiaQ=="
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram{" "}
+            </a>
+          </div>
+          {/* <p>Facebook - facebook.com/posua</p> */}
+          <div>
+            <a
+              href="https://www.facebook.com/nitsposua?mibextid=ZbWKwL"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook{" "}
+            </a>
+          </div>
+        </div>
+        <div className={styles.mail}>
+          <div className="email" style={{ textDecoration: "underline" }}>
+            Mail
+          </div>
+          <div>
+            {" "}
+            <a href="mailto:borhomthuri.posua@gmail.com">borhomthuri.posua@gmail.com</a>
+          </div>
+        </div>
       </div>
+
+      {/* <img src='https://res.cloudinary.com/dclhahfvz/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1710923131/footer_final_mj5rba.jpg?_s=public-apps' alt=''/> */}
     </footer>
   );
 };
--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,180 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Navbar.module.scss";
+import "./fonts.scss";
 const Navbar = () => {
-  return <div className={styles.main}>Navbar</div>;
+  const [selectedItem, setSelectedItem] = useState(null);
+  // const [eventsClicked, setEventsClicked] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const [windowSize, setWindowSize] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleSize = () => {
+        setWindowSize(window.innerWidth);
+      };
+
+      window.addEventListener("resize", handleSize);
+      handleSize();
+
+      return () => {
+        window.removeEventListener("resize", handleSize);
+      };
+    }
+    return () => {};
+  }, []);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    // if (item === "Events") {
+    // setEventsClicked(true);
+    // } else {
+    // setEventsClicked(false);
+    // }
+    closeMenu();
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    // <div className={styles.navbar}>
+    <div
+      className={`${styles.navbar} ${selectedItem === "Events" ? styles.eventsClicked : ""}`}
+    >
+      <div className={styles.links}>
+        <NavItem
+          text="Home"
+          selected={selectedItem === "Home"}
+          onClick={() => handleItemClick("Home")}
+          // eventsClicked={eventsClicked}
+        />
+        <NavItem
+          text="Events"
+          selected={selectedItem === "Events"}
+          onClick={() => handleItemClick("Events")}
+          // eventsClicked={eventsClicked}
+        />
+        <NavItem
+          text="Gallery"
+          selected={selectedItem === "Gallery"}
+          onClick={() => handleItemClick("Gallery")}
+          // eventsClicked={eventsClicked}
+        />
+        <img
+          src={
+            selectedItem === "Events"
+              ? "https://res.cloudinary.com/dclhahfvz/image/upload/v1710922616/posua_logo_pscvmf.png"
+              : "https://res.cloudinary.com/dclhahfvz/image/upload/v1710960996/navbar_logo_xv78qn.png"
+          }
+          alt="Logo"
+          className={styles.logo}
+          // className={`${styles.logo} ${selectedItem === "Events" ? styles.logoWithBackground : ""}`}
+        />{" "}
+        {/* Logo */}
+        {/* <div className={styles.separator}></div> */}
+        <NavItem
+          text="Artists"
+          selected={selectedItem === "Artists"}
+          onClick={() => handleItemClick("Artists")}
+          // eventsClicked={eventsClicked}
+        />
+        <NavItem
+          text="Sponsor"
+          selected={selectedItem === "Sponsor"}
+          onClick={() => handleItemClick("Sponsor")}
+          // eventsClicked={eventsClicked}
+        />
+        <NavItem
+          text="Team"
+          selected={selectedItem === "Team"}
+          onClick={() => handleItemClick("Team")}
+          // eventsClicked={eventsClicked}
+        />
+      </div>
+      <div className={`${styles.mobileMenu} ${isMenuOpen ? styles.open : ""}`}>
+        <div
+          className={`${styles.hamburgerMenu} mr-[80vw] md:mr-0  `}
+          onClick={toggleMenu}
+        >
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
+        {windowSize < 768 && (
+          <img
+            src="https://res.cloudinary.com/dclhahfvz/image/upload/v1710960996/navbar_logo_xv78qn.png"
+            alt=""
+            style={{
+              justifyContent: "center",
+              alignItems: "start",
+              position: "relative",
+              top: "-45px",
+              display: "flex",
+            }}
+          />
+        )}
+        <div className={styles.mobileLinks}>
+          {/* <div className=" mr-[80vw] text-[2rem] text-[#E27B4E] font-medium" onClick={closeMenu}>X</div> */}
+          <div className="mr-[80vw] mt-[-10px]" onClick={closeMenu}>
+            <img
+              src="https://res.cloudinary.com/dclhahfvz/image/upload/v1711130341/orangeCross_tqxpal.png"
+              alt="Close Menu"
+              className="w-[30px] h-[25px] font-bold"
+              style={{ filter: "brightness(95%)" }}
+            />
+          </div>
+
+          <NavItem
+            text="Home"
+            selected={selectedItem === "Home"}
+            onClick={() => handleItemClick("Home")}
+          />
+          <NavItem
+            text="Events"
+            selected={selectedItem === "Events"}
+            onClick={() => handleItemClick("Events")}
+          />
+          <NavItem
+            text="Gallery"
+            selected={selectedItem === "Gallery"}
+            onClick={() => handleItemClick("Gallery")}
+          />
+          <NavItem
+            text="Artists"
+            selected={selectedItem === "Artists"}
+            onClick={() => handleItemClick("Artists")}
+          />
+          <NavItem
+            text="Sponsor"
+            selected={selectedItem === "Sponsor"}
+            onClick={() => handleItemClick("Sponsor")}
+          />
+          <NavItem
+            text="Team"
+            selected={selectedItem === "Team"}
+            onClick={() => handleItemClick("Team")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const NavItem = ({ text, selected, onClick }) => {
+  return (
+    <div
+      className={`${styles.navItem} ${selected ? styles.selected : ""}`}
+      onClick={onClick}
+    >
+      {text}
+    </div>
+  );
 };
 
 export default Navbar;
