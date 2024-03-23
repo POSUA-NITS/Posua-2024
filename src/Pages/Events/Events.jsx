f4f637133a488abbd6d27ba752ae3e92d7222280
hereisadi <hereisadi@proton.me>
2024-03-23 15:34:39 +0530

    wip

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -1,9 +1,51 @@
-import { React, useState } from "react";
+/* eslint-disable no-console */
+import { React, useState, useEffect } from "react";
 import styles from "./Events.module.scss";
 import data from "../../assets/Events.json";
+// import {  } from "react";
 // width: `${current === item.id ? '100vw' : '12rem'}`,
 // height: `${current === item.id ? '100vh' : '12rem'}`,
 const ExtendedView = ({ mode, current, setCurrent }) => {
+  // console.log(mode, current);
+  console.log("mode", mode);
+  console.log("current", current);
+  console.log("src", data[current].src);
+  console.log("typeof current", typeof current);
+  console.log("typeof data.length", typeof data.length);
+  const [allIndex, setAllIndex] = useState([]);
+
+  useEffect(() => {
+    // const arr = []
+    // for (let i = 0; i < data.length; i += 1) {
+    //   if (i !== current - 1) {
+    //     arr.push(i)
+    //   }
+    // }
+    // setAllIndex(arr)
+    if (current === 0) {
+      setAllIndex([1, 2, 3]);
+    } else if (current === 1) {
+      setAllIndex([2, 3, 0]);
+    } else if (current === 2) {
+      setAllIndex([3, 0, 1]);
+    } else if (current === 3) {
+      setAllIndex([0, 1, 2]);
+    }
+  }, [current]);
+
+  console.log("allIndex", allIndex);
+  console.log("data.length", data.length);
+
+  const [thatIndexData, setThatIndexData] = useState([]);
+  useEffect(() => {
+    const arr = [];
+    allIndex.forEach((index) => {
+      arr.push(data[index]);
+    });
+    setThatIndexData(arr);
+  }, [allIndex]);
+  console.log("thatIndexData", thatIndexData);
+
   return (
     <div
       className={styles.ExtendedView}
@@ -17,10 +59,11 @@ const ExtendedView = ({ mode, current, setCurrent }) => {
     >
       <div className={styles.innerFilter}>
         <div className={styles.writtenContent}>
-          <h1 className={styles.h1}>{data[current - 1].name}</h1>
-          <p className={styles.description}>{data[current - 1].description}</p>
+          <h1 className={styles.h1}>{data[current].name}</h1>
+          <p className={styles.description}>{data[current].description}</p>
           <div className={styles.sliderBtnCont}>
-            {current > 1 && (
+            {/* previous button */}
+            {current > 0 && (
               <button
                 onClick={() => setCurrent(current - 1)}
                 className={styles.sliderBtn}
@@ -31,7 +74,8 @@ const ExtendedView = ({ mode, current, setCurrent }) => {
                 />
               </button>
             )}
-            {current < data.length && (
+            {/* forward button */}
+            {current !== data.length - 1 && (
               <button
                 onClick={() => setCurrent(current + 1)}
                 className={styles.sliderBtn}
@@ -44,28 +88,44 @@ const ExtendedView = ({ mode, current, setCurrent }) => {
             )}
           </div>
         </div>
-        <div className={styles.carouselParent}>
-          {data.map((item) => (
-            <img
-              key={item.id}
-              src={item.src}
-              alt={item.name}
-              style={{
-                transform: `translateX(${-(current - 1) * 2}%)`,
-                transition: "linear 1000ms",
-                scale: `${current === item.id ? 15 : 1}`,
-              }}
-              className={styles.eachItem}
-            />
-          ))}
+
+        {/* show all the events card except the current one */}
+        <div
+          className={styles.carouselParent}
+          style={{
+            background: `url(${data[current].src}) no-repeat center / cover`,
+            transition: "linear 500ms",
+            // transform: `translateX(${-(current - 1) * 100}vw)`,
+          }}
+        >
+          {/* {thatIndexData.map((item) => ( */}
+          <main id={styles.carouselcardholder}>
+            {thatIndexData.map((item) => (
+              <img
+                key={item.id}
+                src={item.src}
+                alt={item.name}
+                style={
+                  {
+                    // transform: `translateX(${-(current - 1) * 2}%)`,
+                    // transition: "linear 1000ms",
+                    // scale: `${current === item.id - 1 ? 15 : 1}`,
+                    //   display: `${current === item.id ? "none" : "block"}`,
+                  }
+                }
+                className={styles.eachItem}
+              />
+            ))}
+          </main>
         </div>
       </div>
     </div>
   );
 };
+
 const Events = () => {
   const [extended, setExtended] = useState(1);
-  const [current, setCurrent] = useState(1);
+  const [current, setCurrent] = useState(0);
   return (
     <div className={styles.superParent}>
       <div className={styles.parent}>
