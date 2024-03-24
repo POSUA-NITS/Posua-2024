7894c712cfab7f32fd7b9b9a0a9e37e5688f4749
hereisadi <hereisadi@proton.me>
2024-03-24 19:52:29 +0530

    ADD:404

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -4,5 +4,5 @@ import Team from "./Team/Team";
 import Sponsors from "./Sponsors/Sponsors";
 import Borhomthuri from "./Borhomthuri/Borhomthuri";
 import Gallery from "./Gallery/Gallery";
-
-export { Events, Home, Team, Sponsors, Borhomthuri, Gallery };
+import NotFound from "./NotFound/NotFound";
+export { Events, Home, Team, Sponsors, Borhomthuri, Gallery, NotFound };
