09a386ebbe968bbfe805f632ba263a96957266db
kaustav <devkaustav769@gmail.com>
2024-03-25 07:18:22 +0530

    responsiveness of the lower cards

--- src/Pages/Sponsors/Sponsors.jsx
+++ src/Pages/Sponsors/Sponsors.jsx
@@ -13,11 +13,36 @@ const SponsorPage = () => {
             />
           </div>
           <div className="sponsorContainer">
-            <div className="sponsor"></div>
-            <div className="sponsor"></div>
-            <div className="sponsor"></div>
-            <div className="sponsor"></div>
-            <div className="sponsor"></div>
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098244/Hyundai_Logo_zqyig4.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098243/Pran_Logo_qkebp4.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098241/Oil_India-Logo_zwjxl7.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711098241/NRL_Logo_sja6jq.png"
+                alt=""
+              />
+            </div>
+            <div className="sponsor" id="Indian_Oil">
+              <img
+                src="https://res.cloudinary.com/dex25uzgp/image/upload/v1711099239/Indian_Oil_Logo1_gx89of.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
         <div className="second">
@@ -80,6 +105,7 @@ const SponsorPage = () => {
                   National Institute of Technology,Silchar,an institute of national
                   importance is pleased to announce that the institute is organising
                   POSUA’22, the Rongali Bihu Festival of Assam.
+                  <br />
                   <br />
                   The entire team of POSU 2024 is dedicated to ensure maximum reach of our
                   sponsors through the basic framework of the festival. We will make the
