69af6c83752acf1527f6de59f0746bf2c3414a06
Arindom <arindom956@gmail.com>
2024-03-27 20:45:13 +0530

    feat : Borhomthuri

--- src/Components/Borhomthuri/Borhomthuri.jsx
+++ src/Components/Borhomthuri/Borhomthuri.jsx
@@ -1,27 +1,50 @@
-import Pepa from "./Pepa";
 import styles from "./Borhomthuri.module.scss";
 // import Film from "./Filmy";
 // import Card from "./Card";
 import data from "../../assets/Edition.json";
+import LottieHeading from "../LottieHeading/LottieHeading";
 // import ScrolBar from "./ScrolBar";
 // import FrontCover from "./FrontCover";
 const Borhomthuri = () => {
   return (
     <div className={styles.superParent}>
       <div className={styles.ParentClass}>
-        <Pepa />
+        {/* <Pepa /> */}
+        <div id={styles.pepaholder}>
+          <LottieHeading title="BORHOMTHURI" />
+        </div>
         {/* <Film /> */}
         {/* <Card/> */}
         {/* <FrontCover/> */}
         <div className={styles.Child1}>
           <div className={styles.text}>
-            <p>
+            <p className={styles.p}>
               Borhomthuri is the annual souvenir of POSUA.It enfolds indispensable parts
               of Assamese Culture and also the emotions of students in the form of
               enthralling articles,poems,stories and sketches.Borhomthuri reflects the
               ardour of NITS Family towards POSUA and provides an opportunity for the
               literary skills of students to get revived
             </p>
+            <div className={styles.galleryBorhomthuri}>
+              {data.map((item) => (
+                <a
+                  href={item.link}
+                  rel="noopener"
+                  target="_blank"
+                  style={{ margin: "1.2rem" }}
+                >
+                  <img src={item.src} alt="magazine" />
+                  <img
+                    src={item.edition_num}
+                    alt="Edition"
+                    style={{
+                      margin: "0 auto",
+                      padding: ".5rem",
+                    }}
+                  />
+                </a>
+              ))}
+            </div>
           </div>
 
           <div className={styles.cameraImg}>
@@ -42,6 +65,26 @@ const Borhomthuri = () => {
               literary skills of students to get revived
             </p>
           </div>
+          <div className={styles.galleryBorhomthurimobile}>
+            {data.map((item) => (
+              <a
+                href={item.link}
+                rel="noopener"
+                target="_blank"
+                style={{ margin: "1.2rem" }}
+              >
+                <img src={item.src} alt="magazine" />
+                <img
+                  src={item.edition_num}
+                  alt="Edition"
+                  style={{
+                    margin: "0 auto",
+                    padding: ".5rem",
+                  }}
+                />
+              </a>
+            ))}
+          </div>
           {/* <ScrolBar /> */}
           {/* <div className={styles.movingCards}>
             {
@@ -50,14 +93,10 @@ const Borhomthuri = () => {
               ))
             }
           </div> */}
-          <div className={styles.galleryBorhomthuri}>
-            {data.map((item) => (
-              <a href={item.link} style={{ margin: "1.2rem" }}>
-                <img src={item.src} alt="magazine" />
-                <img src={item.edition_num} alt="Edition" />
-              </a>
-            ))}
-          </div>
+
+          {/* <div id={styles.random}>
+<img src="https://res.cloudinary.com/dhpqjrbha/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1711056519/Group_2608667_reshtk.jpg?_s=public-apps" alt="" />
+          </div> */}
           {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ratione, perspiciatis vitae sit asperiores quia sunt quasi maxime. Nihil, officia qui fuga reprehenderit obcaecati ipsam alias vel hic laudantium pariatur aliquam mollitia reiciendis explicabo? */}
         </div>
       </div>
